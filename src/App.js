f3c7ea39e2d90786bd59190edcf0078dfc048d8a
Irad Doron <irad.doron@gmail.com>
2022-06-19 15:18:59 +0300

    fix min height

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Routing from './components/Routing/Routing';
 import MainNav from './components/MainNav/MainNav';
 
-import { Box } from '@mui/material';
+import { Box, Stack } from '@mui/material';
 
 function App() {
   return (
@@ -10,10 +10,14 @@ function App() {
         sx={{
           background:
             'linear-gradient(17deg, rgba(131,58,180,0.2) 0%, rgba(49,135,224,0.2) 50%, rgba(130,69,252,0.2) 100%)',
+          height: '100%',
+          minHeight: '100vh',
         }}
       >
-        <MainNav />
-        <Routing />
+        <Stack direction="column" sx={{ alignItems: 'center' }}>
+          <MainNav />
+          <Routing />
+        </Stack>
       </Box>
     </>
   );
--- src/pages/Courses/RenderLesson/RenderLesson.jsx
+++ src/pages/Courses/RenderLesson/RenderLesson.jsx
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import courses from '../../../data/courses/courses';
 import { NavLink, Outlet } from 'react-router-dom';
+import { Stack, Typography, Button, Divider, Portal } from '@mui/material';
 
 import renderElements from '../../../helpers/renderElement';
 
@@ -14,15 +15,13 @@ function RenderLesson() {
   }, [lesson, chapter]);
 
   return (
-    <div>
-      <div>
-        {lessonData?.lessonElements?.map((element, index) => {
-          const elementName = Object.keys(element)[0];
-          const elementData = element[elementName];
-          return renderElements(elementName, elementData, index);
-        })}
-      </div>
-    </div>
+    <Stack direction="column" sx={{ alignItems: 'center' }}>
+      {lessonData?.lessonElements?.map((element, index) => {
+        const elementName = Object.keys(element)[0];
+        const elementData = element[elementName];
+        return renderElements(elementName, elementData, index);
+      })}
+    </Stack>
   );
 }
 export default RenderLesson;
